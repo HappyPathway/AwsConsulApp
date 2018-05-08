0c4f44f849f356048836be78228239eee8cd5d54
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-08 12:32:26 -0700

    updating

--- opsconfig.tf
+++ opsconfig.tf
@@ -3,5 +3,6 @@ module "opsconfig" {
     triggers = "${module.aws.instance_ids}"
     hosts = "${module.aws.instance_ips}"
     service_name = "${var.service_name}"
+    service_version = "${var.service_version}"
     env = "${var.env}"
 }
