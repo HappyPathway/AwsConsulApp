979caba8b3570d33770c0f19adc61fc2d6a37be1
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-08 19:23:06 -0700

    removing opsconfig and updating removing apt check from config.tf

--- opsconfig.tf
+++
@@ -1,8 +0,0 @@
-module "opsconfig" {
-    source = "git@github.com:HappyPathway/OpsConfig.git"
-    triggers = "${module.aws.instance_ids}"
-    hosts = "${module.aws.instance_ips}"
-    service_name = "${var.service_name}"
-    service_version = "${var.service_version}"
-    env = "${var.env}"
-}
