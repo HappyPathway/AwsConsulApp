4c646c17d406b2d1b1c9ca1500b5e4922671b833
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-10 16:13:32 -0700

    trying to add dns

--- dns.tf
+++ dns.tf
@@ -9,7 +9,7 @@ resource "aws_route53_record" "service" {
 resource "aws_route53_record" "service_nodes" {
   count   = "${length(module.aws.instance_ips)}"
   zone_id = "${data.consul_keys.dns.var.dns_zone}"
-  name    = "${var.service_name}-${var.env}-${var.service_version}.${data.consul_keys.dns.var.dns_name}"
+  name    = "${var.service_name}-${var.env}-${var.service_version}-${count.index}.${data.consul_keys.dns.var.dns_name}"
   type    = "CNAME"
   ttl     = "300"
   records = ["${element(module.aws.instance_ips, count.index)}"]
