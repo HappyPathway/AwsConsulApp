ab20626819506f2093d11cf9519ca0d2d4970b3a
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-11 11:01:57 -0700

    fixing syntax error

--- dns.tf
+++ dns.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "service" {
 }
 
 resource "aws_route53_record" "service_nodes" {
-  count   = "${var.count)}"
+  count   = "${var.count}"
   zone_id = "${data.consul_keys.dns.var.dns_zone}"
   name    = "${var.service_name}-${var.env}-${var.service_version}-${count.index}.${data.consul_keys.dns.var.dns_name}"
   type    = "A"
